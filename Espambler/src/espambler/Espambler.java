ba649b696e040f6e680f5460025e8466b66be17a
Jhonatan <jhonatanstivendbz@hotmail.com>
2021-11-24 12:40:55 -0500

    first commit

---
+++ Espambler/src/espambler/Espambler.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package espambler;
+
+import vistas.Index;
+
+/**
+ *
+ * @author Jhona
+ */
+public class Espambler {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Index i = new Index();
+        i.setVisible(true);
+    }
+    
+}
